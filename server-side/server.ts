d7e907d3b2079cd756cac177ff20f2e45466b464
Andrei Timofeev <andreit@wix.com>
2023-09-03 13:23:24 +0300

    Added some routes

--- server-side/server.ts
+++ server-side/server.ts
@@ -1,17 +1,59 @@
-const fs = require('fs')
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// GET endpoint for retrieving menu data
 app.get('/api/menu', (req, res) => {
-    const data = fs.readFileSync('menus.json')
-    res.send(data)
-})
+    fs.readFile('menus.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Error reading menus.json' });
+        } else {
+            const json = JSON.parse(data);
+            res.json(json);
+        }
+    });
+});
+
+// POST endpoint for modifying menu data (Example: Append data to menus.json)
+app.post('/api/menu', (req, res) => {
+    fs.readFile('menus.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Error reading menus.json' });
+        } else {
+            const menus = JSON.parse(data);
+
+            const newId = Date.now()
+
+            for (let i = 0; i < menus.length; i++) {
+                if (menus[i].id === req.json().parentId) {
+                    menus[i].submenus.push(newId)
+                }
+            }
+
+            menus.push({
+                id: newId,
+                name: req.json().name,
+                submenus: [],
+            });
+
+            fs.writeFile('menus.json', JSON.stringify(menus), 'utf8', (err) => {
+                if (err) {
+                    res.status(500).json({ error: 'Error writing menus.json' });
+                } else {
+                    res.json({ message: 'Menu data updated successfully' });
+                }
+            });
+        }
+    });
+});
+
 app.listen(8080, () => {
-    console.log('Listening on port 8080')
-})
+    console.log('Listening on port 8080');
+});
