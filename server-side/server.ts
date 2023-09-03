5f47b903d22b15251e2ee7fb56a729652faf88e6
Andrei Timofeev <andreit@wix.com>
2023-09-03 14:26:20 +0300

    Updated DELETE

--- server-side/server.ts
+++ server-side/server.ts
@@ -51,8 +51,24 @@ app.post(BASE_URL, (req, res) => {
         }
     });
 });
+
+const deleteMenu = (menusArr, menuId) => {
+    const arrCopy = [...menusArr]
+    for (let i = 0; i < arrCopy.length; i++) {
+        if (arrCopy[i].id == menuId) {
+            if (arrCopy[i].submenus.length > 0) {
+                for (let j = 0; j < arrCopy[i].submenus.length; j++) {
+                    deleteMenu(arrCopy[i], arrCopy[i].submenus[j])
+                }
+            }
+            arrCopy.splice(i, 1)
+        }
+    }
+    return arrCopy
+}
 app.delete(BASE_URL + ':menuId', (req, res) => {
     const menuId = req.params.menuId;
+    let newMenus = []
     fs.readFile('menus.json', 'utf8', (err, data) => {
         if (err) {
             res.status(500).json({ error: 'Error reading menus.json' });
@@ -60,16 +76,14 @@ app.delete(BASE_URL + ':menuId', (req, res) => {
             const menus = JSON.parse(data);
 
             for (let i = 0; i < menus.length; i++) {
-                if (menus[i].id == menuId) {
-                    menus.splice(i, 1)
-                }
+                newMenus = deleteMenu(menus, menuId)
             }
 
-            fs.writeFile('menus.json', JSON.stringify(menus), 'utf8', (err) => {
+            fs.writeFile('menus.json', JSON.stringify(newMenus), 'utf8', (err) => {
                 if (err) {
                     res.status(500).json({ error: 'Error writing menus.json' });
                 } else {
-                    res.json(menus);
+                    res.json(newMenus);
                 }
             });
         }
